d8aacefae1ffcfe3002d44e8d27b8f2a6e814e7f
Ed Luzgin <edward.luzgin@gmail.com>
2022-02-14 11:42:43 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+	return <header className="header"  >
+		<img src='//upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' />
+	</header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+	return <main className="content">
+		<div>
+			<img src="https://cdnn21.img.ria.ru/images/156341/62/1563416260_0:160:3072:1888_1920x0_80_0_0_1895b26177aeb3fab013d096b52cb400.jpg" />
+		</div>	
+		<div>
+			<img src='https://media.istockphoto.com/photos/athlete-staring-intensely-picture-id1280239107?k=20&m=1280239107&s=612x612&w=0&h=aOQ8a2poib4xJqymP8iNvUUwkymSkh-Erii_zCcLiAg=' />
+		</div>
+		<div>
+			my post
+			<div>
+				new post
+			</div>
+			<div>
+				post 1
+			</div>
+			<div>
+				post 2
+			</div>
+		</div>
+	</main>
+}
+
+export default Profile;
